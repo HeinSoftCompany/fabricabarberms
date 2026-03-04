53031086a840fdf1f1563ae1b9f2d6c549a2aba2
Devsphz <devsphz@gmail.com>
2026-03-03 22:06:52 -0300

    Footer upload Logo

--- src/App.tsx
+++ src/App.tsx
@@ -78,7 +78,6 @@ function App() {
     </span>
   </div>
 
-          {/* Nome no DESKTOP (lg pra cima) */}
           {/* TABLET + DESKTOP (UMA LINHA) */}
   <span className="hidden sm:inline-block font-bold text-base md:text-lg tracking-tight">
     FÁBRICA <span className="text-[#F97316]">DE</span> BARBEIROS{' '}
@@ -515,11 +514,15 @@ function App() {
           <div className="max-w-6xl mx-auto">
             <div className="flex flex-col md:flex-row items-center justify-between gap-6">
               <div className="flex items-center gap-3">
-                <div className="w-10 h-10 rounded-full bg-gradient-to-br from-[#F97316] to-[#EF4444] flex items-center justify-center">
-                  <Scissors className="w-5 h-5 text-white" />
-                </div>
+                <div className="w-[56px] h-[56px] rounded-full bg-gradient-to-br from-[#F97316] to-[#EF4444] flex items-center justify-center">
+            <img
+              src={Logo}
+              alt="Fábrica de Barbeiros MS"
+              className="w-[64px] h-[64px] object-contain"
+            />
+          </div>
                 <span className="font-bold text-lg">
-                  FÁBRICA<span className="text-[#F97316]">DE</span>BARBEIROS
+                  FÁBRICA <span className="text-[#F97316]">DE </span>BARBEIROS <span className="text-[#F97316]">MS </span>
                 </span>
               </div>
 
